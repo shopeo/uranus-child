ce7f1874cb854c5a4d4a74599cfe7e252db795aa
jabber <2898117012@qq.com>
2022-07-28 04:55:48 +0800

    init

---
+++ README.md
@@ -0,0 +1,44 @@
+# Uranus theme by wordpress
+
+## Integrated
+
+- [x] bootstrap
+- [x] fontawesome-free
+- [x] animate.css
+- [x] alpinejs
+- [x] @alpinejs/mask
+- [x] @alpinejs/intersect
+- [x] @alpinejs/persist
+- [x] @alpinejs/focus
+- [x] @alpinejs/collapse
+- [x] @alpinejs/morph
+- [x] @ryangjchandler/alpine-clipboard
+- [ ] typed.js
+- [ ] lazysizes
+- [ ] @ciar4n/izmir
+- [ ] hover.css
+- [ ] swiper
+- [ ] webkul-micron
+- [ ] counterup2
+
+## Plugins
+
+- [ ] https://www.sliderrevolution.com/
+- [ ] https://layerslider.com/
+- [ ] https://memberpress.com/
+- [ ] aifanfan
+- [ ] sendcloud-newsletter
+- [ ] wp-multilang
+- [ ] WooCommerce
+- [ ] WooCommerce Memberships
+- [ ] Easy Table of Contents
+- [ ] Heroic Table of Contents
+- [ ] Ultimate Blocks
+- [ ] Hubspot
+- [ ] All in One SEO
+
+## About
+
+## Ref
+
+https://github.com/TGMPA/TGM-Plugin-Activation/
---
+++ theme/functions.php
@@ -0,0 +1,78 @@
+<?php
+
+if ( ! function_exists( 'uranus_child_styles' ) ) {
+	function uranus_child_styles() {
+		$theme_version = wp_get_theme()->get( 'Version' );
+		wp_enqueue_style( 'uranus-child-style', get_stylesheet_uri(), array( 'uranus-style' ), $theme_version );
+		wp_style_add_data( 'uranus-child-style', 'rtl', 'replace' );
+//		 Add print css
+		wp_enqueue_style( 'uranus-child-print-style', get_stylesheet_directory_uri() . '/print.css', array(), $theme_version, 'print' );
+	}
+}
+
+add_action( 'wp_enqueue_scripts', 'uranus_child_styles' );
+
+if ( ! function_exists( 'uranus_child_scripts' ) ) {
+	function uranus_child_scripts() {
+		$theme_version = wp_get_theme()->get( 'Version' );
+		wp_enqueue_script( 'uranus-child-script', get_stylesheet_directory_uri() . '/assets/js/app.js', array( 'jquery' ), $theme_version );
+		wp_script_add_data( 'uranus-child-script', 'async', true );
+	}
+}
+
+add_action( 'wp_enqueue_scripts', 'uranus_child_scripts' );
+
+if ( ! function_exists( 'uranus_child_block_editor_styles' ) ) {
+	function uranus_child_block_editor_styles() {
+		wp_enqueue_style( 'uranus-child-block-editor-styles', get_theme_file_uri( '/assets/css/editor-block.css' ), array(), wp_get_theme()->get( 'Version' ), 'all' );
+		wp_style_add_data( 'uranus-child-block-editor-styles', 'rtl', 'replace' );
+
+		wp_enqueue_script( 'uranus-child-block-editor-script', get_theme_file_uri( '/assets/js/editor-script-block.js' ), array(
+			'wp-blocks',
+			'wp-dom'
+		), wp_get_theme()->get( 'Version' ), true );
+	}
+}
+
+add_action( 'enqueue_block_editor_assets', 'uranus_child_block_editor_styles', 1, 1 );
+
+
+if ( ! function_exists( 'uranus_child_classic_editor_styles' ) ) {
+	function uranus_child_classic_editor_styles() {
+		$classic_editor_styles = array(
+			'/assets/css/editor-classic.css',
+		);
+
+		add_editor_style( $classic_editor_styles );
+	}
+}
+
+add_action( 'init', 'uranus_child_classic_editor_styles' );
+
+if ( ! function_exists( 'uranus_child_customize_controls_enqueue_scripts' ) ) {
+	function uranus_child_customize_controls_enqueue_scripts() {
+		$theme_version = wp_get_theme()->get( 'Version' );
+		wp_enqueue_script( 'uranus-child-customize', get_stylesheet_directory_uri() . '/assets/js/customize.js', array( 'jquery' ), $theme_version, false );
+		wp_enqueue_script( 'uranus-child-customize-controls', get_stylesheet_directory_uri() . '/assets/js/customize-controls.js', array(
+			'uranus-color-calculations',
+			'customize-controls',
+			'underscore',
+			'jquery'
+		), $theme_version, false );
+	}
+}
+
+add_action( 'customize_controls_enqueue_scripts', 'uranus_child_customize_controls_enqueue_scripts' );
+
+if ( ! function_exists( 'uranus_child_customize_preview_init' ) ) {
+	function uranus_child_customize_preview_init() {
+		$theme_version = wp_get_theme()->get( 'Version' );
+		wp_enqueue_script( 'uranus-child-customize-preview', get_theme_file_uri( '/assets/js/customize-preview.js' ), array(
+			'customize-preview',
+			'customize-selective-refresh',
+			'jquery'
+		), $theme_version, true );
+	}
+}
+
+add_action( 'customize_preview_init', 'uranus_child_customize_preview_init' );
